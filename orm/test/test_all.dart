5b87f3aa5188e456841b26244a91fd893e2955f5
Ravi Teja Gudapati <tejainece@gmail.com>
2018-07-28 19:16:17 +0200

    2.2.x

--- orm/test/test_all.dart
+++ orm/test/test_all.dart
@@ -1,13 +1,10 @@
 // Copyright (c) 2016, teja. All rights reserved. Use of this source code
 // is governed by a BSD-style license that can be found in the LICENSE file.
 
-//TODO import 'package:jaguar_orm/jaguar_orm.dart';
-import 'package:test/test.dart';
+import 'basic/simple.dart' as basicSimple;
+import 'one_to_one/simple.dart' as oneToOneSimple;
 
 void main() {
-  group('A group of tests', () {
-    setUp(() {});
-
-    test('First Test', () {});
-  });
+  basicSimple.main();
+  oneToOneSimple.main();
 }
