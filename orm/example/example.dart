2e8796e6771c209c776b4e3b30dfd87e9f118054
tejainece <tejainece@gmail.com>
2018-09-06 18:22:39 +0200

    Fixes

---
+++ orm/example/example.dart
@@ -0,0 +1,97 @@
+import 'dart:async';
+import 'package:jaguar_query/jaguar_query.dart';
+import 'package:jaguar_orm/jaguar_orm.dart';
+
+part 'example.jorm.dart';
+
+class CartItem {
+  @PrimaryKey(auto: true)
+  int id;
+  double amount;
+  @Column(isNullable: true)
+  String product;
+  int quantity;
+  @BelongsTo(CartBean)
+  int cartId;
+
+  CartItem({this.amount, this.product, this.quantity, this.id});
+  CartItem.make(this.amount, this.product, this.quantity);
+
+  @override
+  String toString() => 'id=$id, amount=$amount, quantity=$quantity, product=$product, cartId=$cartId';
+
+  CartItem copy({int id, int quantity, String product, double amount}) {
+    return CartItem(
+      amount: amount ?? this.amount,
+      product: product ?? this.product,
+      quantity: quantity ?? this.quantity,
+      id: id ?? this.id,
+    );
+  }
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+          other is CartItem && runtimeType == other.runtimeType && amount == other.amount && product == other.product && quantity == other.quantity;
+
+  @override
+  int get hashCode => amount.hashCode ^ product.hashCode ^ quantity.hashCode;
+
+}
+
+@GenBean()
+class CartItemBean extends Bean<CartItem> with _CartItemBean {
+  CartItemBean(Adapter adapter) : super(adapter);
+
+  CartBean _cartBean;
+
+  @override
+  String get tableName => 'cartItem';
+
+  @override
+  CartBean get cartBean => _cartBean ??= CartBean(adapter);
+}
+class Cart {
+  @PrimaryKey(auto: true)
+  int id;
+  @HasMany(CartItemBean)
+  List<CartItem> items;
+  double amount;
+
+  Cart({this.id, this.amount = 0.0, this.items = const []});
+
+  Cart copy({id, amount, items}) {
+    return Cart(
+      id: id ?? this.id,
+      amount: amount ?? this.amount,
+      items: items ?? this.items,
+    );
+  }
+
+  String get amountLabel {
+    //return kCurrencyFormat.format(amount);
+  }
+
+  @override
+  String toString() => 'id=$id, amount=$amount, items=$items';
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) || other is Cart && runtimeType == other.runtimeType && items == other.items && amount == other.amount;
+
+  @override
+  int get hashCode => items.hashCode ^ amount.hashCode;
+}
+
+@GenBean()
+class CartBean extends Bean<Cart> with _CartBean {
+  CartBean(Adapter adapter)
+      : cartItemBean = CartItemBean(adapter),
+        super(adapter);
+
+  @override
+  final CartItemBean cartItemBean;
+
+  @override
+  String get tableName => 'cart';
+}
---
+++ orm/example/example.jorm.dart
@@ -0,0 +1,301 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'example.dart';
+
+// **************************************************************************
+// BeanGenerator
+// **************************************************************************
+
+abstract class _CartItemBean implements Bean<CartItem> {
+  final id = new IntField('id');
+  final amount = new DoubleField('amount');
+  final product = new StrField('product');
+  final quantity = new IntField('quantity');
+  final cartId = new IntField('cart_id');
+  Map<String, Field> _fields;
+  Map<String, Field> get fields => _fields ??= {
+        id.name: id,
+        amount.name: amount,
+        product.name: product,
+        quantity.name: quantity,
+        cartId.name: cartId,
+      };
+  CartItem fromMap(Map map) {
+    CartItem model = new CartItem();
+
+    model.id = adapter.parseValue(map['id']);
+    model.amount = adapter.parseValue(map['amount']);
+    model.product = adapter.parseValue(map['product']);
+    model.quantity = adapter.parseValue(map['quantity']);
+    model.cartId = adapter.parseValue(map['cart_id']);
+
+    return model;
+  }
+
+  List<SetColumn> toSetColumns(CartItem model,
+      {bool update = false, Set<String> only}) {
+    List<SetColumn> ret = [];
+
+    if (only == null) {
+      ret.add(id.set(model.id));
+      ret.add(amount.set(model.amount));
+      ret.add(product.set(model.product));
+      ret.add(quantity.set(model.quantity));
+      ret.add(cartId.set(model.cartId));
+    } else {
+      if (only.contains(id.name)) ret.add(id.set(model.id));
+      if (only.contains(amount.name)) ret.add(amount.set(model.amount));
+      if (only.contains(product.name)) ret.add(product.set(model.product));
+      if (only.contains(quantity.name)) ret.add(quantity.set(model.quantity));
+      if (only.contains(cartId.name)) ret.add(cartId.set(model.cartId));
+    }
+
+    return ret;
+  }
+
+  Future<void> createTable() async {
+    final st = Sql.create(tableName);
+    st.addInt(id.name, primary: true, autoIncrement: true, isNullable: false);
+    st.addDouble(amount.name, isNullable: false);
+    st.addStr(product.name, isNullable: true);
+    st.addInt(quantity.name, isNullable: false);
+    st.addInt(cartId.name,
+        foreignTable: cartBean.tableName, foreignCol: 'id', isNullable: false);
+    return adapter.createTable(st);
+  }
+
+  Future<dynamic> insert(CartItem model) async {
+    final Insert insert = inserter.setMany(toSetColumns(model));
+    return adapter.insert(insert);
+  }
+
+  Future<void> insertMany(List<CartItem> models) async {
+    final List<List<SetColumn>> data =
+        models.map((model) => toSetColumns(model)).toList();
+    final InsertMany insert = inserters.addAll(data);
+    return adapter.insertMany(insert);
+  }
+
+  Future<int> update(CartItem model, {Set<String> only}) async {
+    final Update update = updater
+        .where(this.id.eq(model.id))
+        .setMany(toSetColumns(model, only: only));
+    return adapter.update(update);
+  }
+
+  Future<void> updateMany(List<CartItem> models) async {
+    final List<List<SetColumn>> data = [];
+    final List<Expression> where = [];
+    for (var i = 0; i < models.length; ++i) {
+      var model = models[i];
+      data.add(toSetColumns(model).toList());
+      where.add(this.id.eq(model.id));
+    }
+    final UpdateMany update = updaters.addAll(data, where);
+    return adapter.updateMany(update);
+  }
+
+  Future<CartItem> find(int id,
+      {bool preload: false, bool cascade: false}) async {
+    final Find find = finder.where(this.id.eq(id));
+    return await findOne(find);
+  }
+
+  Future<int> remove(int id) async {
+    final Remove remove = remover.where(this.id.eq(id));
+    return adapter.remove(remove);
+  }
+
+  Future<int> removeMany(List<CartItem> models) async {
+    final Remove remove = remover;
+    for (final model in models) {
+      remove.or(this.id.eq(model.id));
+    }
+    return adapter.remove(remove);
+  }
+
+  Future<List<CartItem>> findByCart(int cartId,
+      {bool preload: false, bool cascade: false}) async {
+    final Find find = finder.where(this.cartId.eq(cartId));
+    return findMany(find);
+  }
+
+  Future<List<CartItem>> findByCartList(List<Cart> models,
+      {bool preload: false, bool cascade: false}) async {
+    final Find find = finder;
+    for (Cart model in models) {
+      find.or(this.cartId.eq(model.id));
+    }
+    return findMany(find);
+  }
+
+  Future<int> removeByCart(int cartId) async {
+    final Remove rm = remover.where(this.cartId.eq(cartId));
+    return await adapter.remove(rm);
+  }
+
+  void associateCart(CartItem child, Cart parent) {
+    child.cartId = parent.id;
+  }
+
+  CartBean get cartBean;
+}
+
+abstract class _CartBean implements Bean<Cart> {
+  final id = new IntField('id');
+  final amount = new DoubleField('amount');
+  Map<String, Field> _fields;
+  Map<String, Field> get fields => _fields ??= {
+        id.name: id,
+        amount.name: amount,
+      };
+  Cart fromMap(Map map) {
+    Cart model = new Cart();
+
+    model.id = adapter.parseValue(map['id']);
+    model.amount = adapter.parseValue(map['amount']);
+
+    return model;
+  }
+
+  List<SetColumn> toSetColumns(Cart model,
+      {bool update = false, Set<String> only}) {
+    List<SetColumn> ret = [];
+
+    if (only == null) {
+      ret.add(id.set(model.id));
+      ret.add(amount.set(model.amount));
+    } else {
+      if (only.contains(id.name)) ret.add(id.set(model.id));
+      if (only.contains(amount.name)) ret.add(amount.set(model.amount));
+    }
+
+    return ret;
+  }
+
+  Future<void> createTable() async {
+    final st = Sql.create(tableName);
+    st.addInt(id.name, primary: true, autoIncrement: true, isNullable: false);
+    st.addDouble(amount.name, isNullable: false);
+    return adapter.createTable(st);
+  }
+
+  Future<dynamic> insert(Cart model, {bool cascade: false}) async {
+    final Insert insert = inserter.setMany(toSetColumns(model))..id(id.name);
+    final ret = await adapter.insert(insert);
+    if (cascade) {
+      Cart newModel;
+      if (model.items != null) {
+        newModel ??= await find(ret);
+        model.items.forEach((x) => cartItemBean.associateCart(x, newModel));
+        for (final child in model.items) {
+          await cartItemBean.insert(child);
+        }
+      }
+    }
+    return ret;
+  }
+
+  Future<void> insertMany(List<Cart> models, {bool cascade: false}) async {
+    if (cascade) {
+      final List<Future> futures = [];
+      for (var model in models) {
+        futures.add(insert(model, cascade: cascade));
+      }
+      return Future.wait(futures);
+    } else {
+      final List<List<SetColumn>> data =
+          models.map((model) => toSetColumns(model)).toList();
+      final InsertMany insert = inserters.addAll(data);
+      return adapter.insertMany(insert);
+    }
+  }
+
+  Future<int> update(Cart model,
+      {bool cascade: false, bool associate: false, Set<String> only}) async {
+    final Update update = updater
+        .where(this.id.eq(model.id))
+        .setMany(toSetColumns(model, only: only));
+    final ret = adapter.update(update);
+    if (cascade) {
+      Cart newModel;
+      if (model.items != null) {
+        if (associate) {
+          newModel ??= await find(model.id);
+          model.items.forEach((x) => cartItemBean.associateCart(x, newModel));
+        }
+        for (final child in model.items) {
+          await cartItemBean.update(child);
+        }
+      }
+    }
+    return ret;
+  }
+
+  Future<void> updateMany(List<Cart> models, {bool cascade: false}) async {
+    if (cascade) {
+      final List<Future> futures = [];
+      for (var model in models) {
+        futures.add(update(model, cascade: cascade));
+      }
+      return Future.wait(futures);
+    } else {
+      final List<List<SetColumn>> data = [];
+      final List<Expression> where = [];
+      for (var i = 0; i < models.length; ++i) {
+        var model = models[i];
+        data.add(toSetColumns(model).toList());
+        where.add(this.id.eq(model.id));
+      }
+      final UpdateMany update = updaters.addAll(data, where);
+      return adapter.updateMany(update);
+    }
+  }
+
+  Future<Cart> find(int id, {bool preload: false, bool cascade: false}) async {
+    final Find find = finder.where(this.id.eq(id));
+    final Cart model = await findOne(find);
+    if (preload) {
+      await this.preload(model, cascade: cascade);
+    }
+    return model;
+  }
+
+  Future<int> remove(int id, [bool cascade = false]) async {
+    if (cascade) {
+      final Cart newModel = await find(id);
+      await cartItemBean.removeByCart(newModel.id);
+    }
+    final Remove remove = remover.where(this.id.eq(id));
+    return adapter.remove(remove);
+  }
+
+  Future<int> removeMany(List<Cart> models) async {
+    final Remove remove = remover;
+    for (final model in models) {
+      remove.or(this.id.eq(model.id));
+    }
+    return adapter.remove(remove);
+  }
+
+  Future<Cart> preload(Cart model, {bool cascade: false}) async {
+    model.items = await cartItemBean.findByCart(model.id,
+        preload: cascade, cascade: cascade);
+    return model;
+  }
+
+  Future<List<Cart>> preloadAll(List<Cart> models,
+      {bool cascade: false}) async {
+    models.forEach((Cart model) => model.items ??= []);
+    await OneToXHelper.preloadAll<Cart, CartItem>(
+        models,
+        (Cart model) => [model.id],
+        cartItemBean.findByCartList,
+        (CartItem model) => [model.cartId],
+        (Cart model, CartItem child) => model.items.add(child),
+        cascade: cascade);
+    return models;
+  }
+
+  CartItemBean get cartItemBean;
+}
