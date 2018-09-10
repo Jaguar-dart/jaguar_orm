1ff85e4e00ca737e6ed3b94355af8976194aba02
Hexah <hexer504@gmail.com>
2018-09-10 21:52:49 +0200

    Update library name

--- sqljocky/lib/src/adapter.dart
+++ sqljocky/lib/src/adapter.dart
@@ -1,7 +1,7 @@
 // Copyright (c) 2016, teja. All rights reserved. Use of this source code
 // is governed by a BSD-style license that can be found in the LICENSE file.
 
-library jaguar_query_postgresql.src;
+library jaguar_orm_sqljocky.src;
 
 import 'dart:async';
 
